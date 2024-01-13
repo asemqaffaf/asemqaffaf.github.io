d18624aa72ab36e8d875dc0ec6aef150dc4aaef8
Asem Qaffaf <asem@qaffaf.com>
2024-01-14 06:35:12 +0900

    fix: image load

--- next.config.js
+++ next.config.js
@@ -1,44 +1,25 @@
 // @ts-check
 
-// const repo = "asemqaffaf.github.io";
-// const assetPrefix = `/${repo}/`;
-// const basePath = `/${repo}`;
-
 /**
  * @type {import('next').NextConfig}
  * */
 const nextConfig = {
-  // /**
-  //  * Enable static exports for the App Router.
-  //  *
-  //  * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-  //  */
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+   */
   output: "export",
 
-  // trailingSlash: true,
-  // skipTrailingSlashRedirect: true,
-
-  // distDir: "dist",
-
-  // /**
-  //  * Set base path. This is usually the slug of your repository.
-  //  *
-  //  * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-  //  */
-  // basePath: "/asemqaffaf.github.io",
-
-  // /**
-  //  * Disable server-based image optimization. Next.js does not support
-  //  * dynamic features with static exports.
-  //  *
-  //  * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-  //  */
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
   images: {
     unoptimized: true,
   },
-
-  // assetPrefix,
-  // basePath,
 };
 
 module.exports = nextConfig;
