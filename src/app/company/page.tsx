c19e26d36a97fea3b0f953e363d3b4de07bb7f2d
Asem Qaffaf <asem@qaffaf.com>
2024-01-18 01:18:20 +0900

    fix: build when there is an error

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -7,7 +7,7 @@ import Company from "@/app/components/pages/company";
 export default function page() {
   return (
     <div className="h-screen w-screen">
-      <Company currentPage={1} />
+      <Company />
     </div>
   );
 }
