54dc9f5af7cffb65fdf07f4e4bc47e2263ac9e09
Asem Qaffaf <asem@qaffaf.com>
2024-01-20 20:25:39 +0900

    fix: landscape mobile mode

--- src/app/components/pages/portfolio-second-page.tsx
+++ src/app/components/pages/portfolio-second-page.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function PortfolioSecondPage() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-end overflow-hidden bg-red-100 py-6" />
+    <div className="flex h-screen w-screen flex-col items-center justify-end overflow-hidden py-6" />
   );
 }
