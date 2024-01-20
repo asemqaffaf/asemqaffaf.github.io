95d670d7155be59e990032faa4ff2b1fcf0a4411
Asem Qaffaf <asem@qaffaf.com>
2024-01-20 20:36:30 +0900

    fix: fix portfolio cards size

--- src/app/components/pages/portfolio.tsx
+++ src/app/components/pages/portfolio.tsx
@@ -1,20 +1,13 @@
 import Image from "next/image";
-import React, { useRef } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 
 export default function Portfolio() {
   const { t } = useTranslation();
-  const containerRef = useRef<HTMLDivElement>(null);
 
-  const hightLogo = Math.floor((containerRef.current?.clientHeight || 100) / 2);
-  const widthLogo = Math.floor((containerRef.current?.clientHeight || 100) / 2);
-  // console.log('widthLogo :>> ', widthLogo);
   return (
-    <div
-      ref={containerRef}
-      className="flex h-full w-screen flex-col bg-zinc-200 pt-24"
-    >
-      <div className="mb-6 flex  w-full justify-center">
+    <div className="flex h-full w-screen flex-col bg-zinc-200 pt-24">
+      <div className="my-6 flex  w-full justify-center">
         <h1 className="z-10 animate-title bg-white bg-clip-text py-4 font-display text-2xl text-zinc-700 duration-1000  sm:text-2xl md:text-4xl">
           {t("page.Portfolio.title")}
         </h1>
@@ -23,8 +16,8 @@ export default function Portfolio() {
         <div className="grid gap-4">
           <div>
             <Image
-              height={hightLogo || 500}
-              width={widthLogo || 500}
+              width={500}
+              height={800}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/expedia-group.webp"
               alt=""
@@ -32,8 +25,8 @@ export default function Portfolio() {
           </div>
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/ntt-docomo.jpg"
               alt=""
@@ -41,8 +34,8 @@ export default function Portfolio() {
           </div>
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full   rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/orange.svg.png"
               alt=""
@@ -50,8 +43,8 @@ export default function Portfolio() {
           </div>
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/WCnTMIyj_400x400.jpg"
               alt=""
@@ -61,8 +54,8 @@ export default function Portfolio() {
         <div className="grid gap-4">
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/benesse.jpg"
               alt=""
@@ -70,8 +63,8 @@ export default function Portfolio() {
           </div>
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full   rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/wired-jp.jpg"
               alt=""
@@ -79,8 +72,8 @@ export default function Portfolio() {
           </div>
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/ww.jpg"
               alt=""
@@ -89,8 +82,8 @@ export default function Portfolio() {
 
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/stricktly-data.png"
               alt=""
@@ -100,8 +93,8 @@ export default function Portfolio() {
         <div className="grid gap-4">
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/expedia.png"
               alt=""
@@ -109,8 +102,8 @@ export default function Portfolio() {
           </div>
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full   rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/open-garden.png"
               alt=""
@@ -118,8 +111,8 @@ export default function Portfolio() {
           </div>
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/nrby.png"
               alt=""
@@ -127,8 +120,8 @@ export default function Portfolio() {
           </div>
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/ripe.png"
               alt=""
@@ -138,8 +131,8 @@ export default function Portfolio() {
         <div className="grid gap-4">
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/vtex.png"
               alt=""
@@ -148,8 +141,8 @@ export default function Portfolio() {
 
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/brick.jpeg"
               alt=""
@@ -157,8 +150,8 @@ export default function Portfolio() {
           </div>
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/wired-jp-share.jpg"
               alt=""
@@ -167,8 +160,8 @@ export default function Portfolio() {
 
           <div>
             <Image
-              height={hightLogo}
-              width={widthLogo}
+              width={500}
+              height={500}
               className="h-auto max-w-full rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
               src="/images/companies/ATG-Logo.png"
               alt=""
