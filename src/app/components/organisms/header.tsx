af01ede7730f841be3d1a0905ea3d1569187f02f
Asem Qaffaf <asem@qaffaf.com>
2024-01-21 18:42:46 +0900

    chore: update header

--- src/app/components/organisms/header.tsx
+++ src/app/components/organisms/header.tsx
@@ -1,27 +1,457 @@
 const Header = () => {
   return (
     <>
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
       <meta charSet="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <link rel="canonical" href="http://asemqaffaf.github.io" />
+      <link href="http://asemqaffaf.github.io" />
+
+      <link href="https://asemqaffaf.github.io/images/asem.png" />
       <meta
-        http-equiv="origin-trial"
+        property="image:alt_text"
+        content="Asem Qaffaf's logo in white background"
+      />
+      <meta property="og:site_name" content="Asem Qaffaf" />
+
+      <meta property="og:url" content="https://asemqaffaf.github.io/" />
+      <meta
+        property="og:title"
+        content="Tech Software &amp; Development | Asem"
+      />
+      <meta
+        property="og:title"
+        content="Electric  Solar &amp; Clean Energy | Asem Qaffaf"
+      />
+
+      <meta
+        property="og:image"
+        content="https://digitalassets-secure.tesla.com/image/upload/f_auto,q_auto/u0rokaqwf96h79cdkfot.jpg"
+      />
+      <meta
+        property="og:image:url"
+        content="https://digitalassets-secure.tesla.com/image/upload/f_auto,q_auto/u0rokaqwf96h79cdkfot.jpg"
+      />
+      <meta
+        property="og:image:secure_url"
+        content="https://digitalassets-secure.tesla.com/image/upload/f_auto,q_auto/u0rokaqwf96h79cdkfot.jpg"
+      />
+      <meta
+        property="og:image:alt"
+        content="Asem logo in gray on white background"
+      />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@asemqaffaf" />
+
+      <meta name="twitter:creator" content="@asemqaffaf" />
+      <meta name="twitter:url" content="https://asemqaffaf.github.io/" />
+      <meta name="twitter:image:alt" content="Asem Qaffaf logo" />
+      <meta
+        name="twitter:image"
+        content="https://digitalassets-secure.tesla.com/image/upload/f_auto,q_auto/u0rokaqwf96h79cdkfot.jpg"
+      />
+
+      <meta name="MobileOptimized" content="width" />
+      <meta name="HandheldFriendly" content="true" />
+
+      <link
+        rel="alternate"
+        hrefLang="x-default"
+        href="https://asemqaffaf.github.io/"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-au"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="de-at"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="nl-be"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="fr-be"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-ca"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="fr-ca"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="cs-cz"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="da-dk"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-us"
+        href="https://asemqaffaf.github.io"
+      />
+      <link rel="alternate" hrefLang="en" href="https://asemqaffaf.github.io" />
+      <link
+        rel="alternate"
+        hrefLang="fi-fi"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="fr-fr"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="de-de"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="el-gr"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="zh-hk"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-hk"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="hu-hu"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="is-is"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-ie"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="it-it"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="ja-jp"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-jo"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="fr-lu"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="de-lu"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="zh-mo"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-mo"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="es-mx"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="nl-nl"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-nz"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="no-no"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-eu"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="pl-pl"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="pt-pt"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-pr"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="es-pr"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="ar-qa"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-qa"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="ro-ro"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-sg"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="sl-si"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="ko-kr"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="es-es"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="sv-se"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="fr-ch"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="de-ch"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="it-ch"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="zh-tw"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-th"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="th-th"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="tr-tr"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="ar-ae"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-ae"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-gb"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="he-il"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="hr-hr"
+        href="https://asemqaffaf.github.io"
+      />
+      <link
+        rel="alternate"
+        hrefLang="en-my"
+        href="https://asemqaffaf.github.io"
+      />
+
+      <link
+        rel="icon"
+        href="/themes/custom/tesla_frontend/assets/favicons/favicon.ico"
+        type="image/vnd.microsoft.icon"
+      />
+
+      <title>Asem Qaffaf Software Engineer</title>
+      <link
+        rel="stylesheet"
+        media="all"
+        href="/_flysystem/s3/css/css_j39t6rX8O2ohivgZfV-7RoSqFys-wpy9JgAK3kHJ4is.css"
+      />
+      <link
+        rel="stylesheet"
+        media="all"
+        href="/_flysystem/s3/css/css_OjwxOO8bhvXSZAMAN4u8s8FW-x1JinYhCLgrN517fU0.css"
+      />
+      <link
+        rel="stylesheet"
+        media="all"
+        href="/_flysystem/s3/css/css_6GaNYruJ7yOU8cW2tQWIY-Rd6Wjbi0ejaxacZs_tG9Y.css"
+      />
+
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      {/* <!-- Lets get mobile --> */}
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      <meta content="True" name="HandheldFriendly" />
+      <meta
+        name="viewport"
+        content="width=device-width, minimum-scale=1.0, initial-scale=1.0, shrink-to-fit=no, viewport-fit=cover"
+      />
+
+      {/* <!-- Favicons and Apple Touch Icons --> */}
+      <meta name="theme-color" content="#000000" />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="196x196"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="160x160"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/logo/favicon.ico"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/logo/favicon.ico"
+      />
+      <link rel="icon" type="image/x-icon" href="/images/logo/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#5635fa" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        httpEquiv="origin-trial"
         data-feature="EME Extension - Policy Check"
         data-expires="2018-11-26"
         content="Aob+++752GiUzm1RNSIkM9TINnQDxTlxz02v8hFJK/uGO2hmXnJqH8c/ZpI05b2nLsHDhGO3Ce2zXJUFQmO7jA4AAAB1eyJvcmlnaW4iOiJodHRwczovL25ldGZsaXguY29tOjQ0MyIsImZlYXR1cmUiOiJFbmNyeXB0ZWRNZWRpYUhkY3BQb2xpY3lDaGVjayIsImV4cGlyeSI6MTU0MzI0MzQyNCwiaXNTdWJkb21haW4iOnRydWV91"
       />
       <title>Asem Qaffaf, Full stack software engineer</title>
-      <link
-        rel="preload"
-        href="https://assets.nflxext.com/web/ffe/wp/signup/nmhp/nmhpFrameworkClient.js.a91110033534342fdd0e.js"
-        as="script"
-      />
-      <link
-        type="text/css"
-        rel="stylesheet"
-        href="/personalization/cl2/freeform/WebsiteDetect?source=wwwhead&amp;fetchType=css&amp;modalView=nmLanding"
-        data-uia="botLink"
-      />
     </>
   );
 };
