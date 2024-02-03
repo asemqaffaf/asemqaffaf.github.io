3dacd70a4db51f3a117bb0d8ecec599349fbb991
Asem Qaffaf <asem@qaffaf.com>
2024-02-03 18:21:26 +0900

    chore: change news rss feed

--- src/components/pages/news.tsx
+++ src/components/pages/news.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-// import Image from "next/image";
+import Image from "next/image";
 import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 
-import useRssParser from "@/hooks/useRssParser";
+import useFetch from "@/hooks/useFetch";
 
-// import useFetch from "@/hooks/useFetch";
+// import useRssParser from "@/hooks/useRssParser";
 import NewsSkeleton from "../molecules/newsSkeleton";
 
 type FeedsType = {
@@ -20,19 +20,18 @@ type FeedsType = {
 
   items: [
     {
-      link: string | undefined;
-      pubDate: string | number | Date;
-      content: string;
+      author: { displayName: string };
+      assetUrl: string;
+      excerpt: string;
+      addedOn: string | number | Date;
+      body: string;
       id: string;
-      url: string;
       title: string;
-      content_text: string;
-      content_html: string;
       image: string;
       date_published: string;
-      authors: [{ name: string }];
-      attachments: string;
-      "dc:creator": string;
+      tags: string[];
+      fullUrl: string;
+      seoData: { seoDescription: string };
     },
   ];
 };
@@ -54,7 +53,7 @@ export default function News() {
     }
   }, [language]);
 
-  const { data, isLoading, error } = useRssParser<FeedsType>(url as string);
+  const { data, isLoading, error } = useFetch<FeedsType>(url as string);
 
   return isLoading || error ? (
     <div className="h-full w-screen py-24 px-5 border-white bg-white  dark:bg-gray-900">
@@ -70,7 +69,7 @@ export default function News() {
             <li className="mb-10 ms-4" key={item.id}>
               <div className="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700" />
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                {new Date(item.pubDate).toLocaleTimeString([], {
+                {new Date(item.addedOn).toLocaleTimeString([], {
                   hour: "2-digit",
                   minute: "2-digit",
                   timeZone: "UTC",
@@ -79,29 +78,35 @@ export default function News() {
               <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                 {item.title}
               </h3>
-              {/* <Image
-                width={400}
-                height={400}
+              <Image
+                width={500}
+                height={500}
                 className="h-auto rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
-                src={item.image}
+                src={item.assetUrl}
                 alt={item.title}
-              /> */}
+              />
               <p className="mb-4 w-3/4 text-base font-normal text-gray-500 lg:w-2/4 dark:text-gray-400">
-                <div dangerouslySetInnerHTML={{ __html: item.content }} />
+                {item.seoData.seoDescription
+                  .replaceAll(`"`, "")
+                  .replaceAll(`“`, "")
+                  .replaceAll(`”`, "")}
               </p>
               <p className="mb-2  text-sm font-normal text-gray-500 dark:text-gray-400">
-                {item["dc:creator"]}
+                {item.tags.reduce((acc: string, tag: string, index: number) => {
+                  // eslint-disable-next-line no-param-reassign
+                  acc += `${tag}${index === item.tags.length - 1 ? " " : ", "}`;
+                  return acc;
+                }, "")}
               </p>
-              {item?.authors?.map((author) => (
-                <p
-                  className="mb-2  text-sm font-normal text-gray-500 dark:text-gray-400"
-                  key={author.name}
-                >
-                  {author.name}
-                </p>
-              ))}
+              <p
+                className="mb-2  text-sm font-normal text-gray-500 dark:text-gray-400"
+                key={item.author.displayName}
+              >
+                {item.author.displayName}
+              </p>
+
               <a
-                href={item.link}
+                href={`https://www.ajplus.net${item.fullUrl}`}
                 target="_blank"
                 className="inline-flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
               >
